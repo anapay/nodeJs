99a2ab5042a2e630ddeb2f3e5608b5ecd4314eeb
anapay <anapay42@gmail.com>
2020-06-16 15:16:25 -0300

    ModulosJs

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+
+var somaFunc = function(a,b){
+  return a+b;
+}
+var subFunc = function(a;b){
+  return a-b;
+}
+var multFunc = function(a,b){
+  return a*b;
+}
+var divFunc = function(a,b){
+  return a/b
+}
