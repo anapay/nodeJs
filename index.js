88c0855f7a04639cd263181f09295ca34fb212ce
anapay <anapay42@gmail.com>
2020-06-17 16:21:39 -0300

    request (paranmetro)

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const app = express();
 app.get("/", function (req, res) {
     
     res.send("welcome to my app!")
+//atravez da função send dentro do objeto res estou enviando uma mensagem
 });
 
 app.get("/on", function (req, res) {
 
-    res.send("about this page")
+    res.send("About this page")
     
 });
 
 app.get("/blog", function (rq, res) {
     res.send("Welcome to my blog!")
+    
+});
+
+app.get("/Hi/:name/:office/:mother", function (req, res) {
+    res.send("<h1>Hi! " +req.params.name+"</h1>"+"<h2>your profession " +req.params.office+ "</h2>"+"<h3>mother " + req.params.mother +"</h3>");
     
 });
 
