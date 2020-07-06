4d4fc17bc827ba10c5161232e2eca1d1532e7ef6
anapay <anapay42@gmail.com>
2020-07-06 16:26:06 -0300

    models mongo

---
+++ trabalahndo com mongo/collections.m
@@ -0,0 +1,5 @@
+ para visualizar as colllections;
+ show colllections
+
+ para visulaizar o conteudo da colllections
+ db nome da colllections.find()
--- trabalahndo com mongo/conecta.js
+++ trabalahndo com mongo/conecta.js
@@ -3,16 +3,57 @@
 const mongoose = require("mongoose")
 
 //configurando monggose
-monggose.Promisse = global.Promise;
-mongoose.connect("mongodb://localhost/teste",{
+mongoose.Promisse = global.Promise;
+mongoose.connect("mongodb://localhost/teste", {
     useMongoClient: true
 
 }).then(() => {
     console.log("Mongodb conectado!");
-    
 
-}).catch((err) =>{
-    console.log("Erro ao se conectar com o banco:"+err);
-    
+
+}).catch((err) => {
+    console.log("Erro ao se conectar com o banco:" + err);
+
+
+})
+//definindo model usuarios
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String,
+        require: false
+    }
+})
+//colletction 
+mongoose.model("usuarios", UsuarioSchema)
+//criando novo usuario 
+const novoUsuario = mongoose.model("usuarios")
+
+new novoUsuario({
+    nome: "Joao",
+    sobrenome: "vitor",
+    email: "Joao@vitor.com",
+    idade: 18,
+    pais: "Brasil"
+}).save().then(() => {
+    console.log("Usuario criado com sucesso")
+
+}).catch((err) => {
+    console.log("Erro ao cadastrrar usuario: " +erro)
 
 })
